d1b9e87c48d1bcd3b49ca66d16131443c032c755
s0568841 <s0568841@htw-berlin.de>
2023-04-19 12:38:08 +0200

    first push, raw spring boot project

---
+++ src/main/java/berlin/htw/webtech/trainingsplan/TrainingsplanApplication.java
@@ -0,0 +1,13 @@
+package berlin.htw.webtech.trainingsplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingsplanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainingsplanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/berlin/htw/webtech/trainingsplan/TrainingsplanApplicationTests.java
@@ -0,0 +1,13 @@
+package berlin.htw.webtech.trainingsplan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingsplanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
